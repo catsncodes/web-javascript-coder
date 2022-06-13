e237494548a4d9ad2c706ac3defceefa6ee03531
Luan Carletto <carlettoluan@gmail.com>
2022-06-13 13:01:31 -0300

    uploading files

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,4 @@
+console.log('a =',a);
+var a = 2; // içamento. o interpretador "joga" a variável
+           // para cima
+console.log('a =',a);
---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,6 @@
+let numero = 1; //pode ser let ou var
+{
+  let numero = 2;
+  console.log('dentro =', numero);
+}
+console.log('dentro =', numero);
