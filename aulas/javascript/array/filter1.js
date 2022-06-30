33503f6d09ee4bef6090a46884d74f2199a623e0
Luan Carletto <carlettoluan@gmail.com>
2022-06-30 19:47:26 -0300

    array part 2 commit

--- aulas/javascript/array/filter1.js
+++ aulas/javascript/array/filter1.js
@@ -1,6 +1,15 @@
 const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'iPad Pro', preco: 4199, fragil: true },
-    { nome: '', preco: 12.49, fragil: true },
-    { nome: 'Notebook', preco: 18.99, fragil: false },
+    { nome: 'Copo de vidro', preco: 18.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 12.99, fragil: false },
 ]
+
+console.log(produtos.filter(function(p) {
+        return false; // todos os elementos são excluidos
+}));
+
+const caro = produto => produto.preco >= 500;
+const fragil = produto => produto.fragil;
+
+console.log(produtos.filter(caro).filter(fragil));
